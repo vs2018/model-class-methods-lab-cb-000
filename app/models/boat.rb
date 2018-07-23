c24ec7265e2e1ca3f9a93ba8cefd3ea5a0ffe0ea
Vishal Sharma <vsh1819@gmail.com>
2018-07-23 13:23:31 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -39,6 +39,8 @@ self.joins(:classifications).where(classifications: {name: "Sailboat"})
         boats = ["Nacra 17", "Zodiac CZ7", "Sun Tracker Regency 254 XP3"].sort
 
       one = Classification.joins(:boats).where(boats: {name: "Nacra 17"})
+      two = Classification.joins(:boats).where(boats: {name: "Zodiac CZ7"})
+      three = Classification.joins(:boats).where(boats: {name: "Sun Tracker Regency 254 XP3"})
 
    
     self.joins(:classifications).where(classifications: {name: ["Sailboat", "Ketch", "Catamaran"]})
