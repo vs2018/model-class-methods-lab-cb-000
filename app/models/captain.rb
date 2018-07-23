447040ef448ce876e2da951f5b828358af0d8564
Vishal Sharma <vsh1819@gmail.com>
2018-07-23 13:20:39 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,24 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  
+  def self.catamaran_operators
+    self.joins(boats: :classifications).where(classifications: {name: "Catamaran"})
+
+  end
+  
+  def self.sailors
+    self.joins(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+
+  end
+  
+  def self.talented_seafarers
+        self.joins(boats: :classifications).where(classifications: {name: ["Sailboat", "Motorboat"]})
+
+  end
+  
+  def self.non_sailors
+    
+  end
+  
+  
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,16 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+  
+  
+  def self.my_all
+    self.all
+  end
+  
+  def self.longest
+    lengthL = Boat.maximum('length')
+
+    self.joins(:boats).where(boats: {length: lengthL})
+
+  end
 end
