a6869e72b9f1005dfb4a5edafe7e3f5c67a291f0
Vishal Sharma <vsh1819@gmail.com>
2018-07-23 13:29:06 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -36,13 +36,13 @@ self.joins(:classifications).where(classifications: {name: "Sailboat"})
   end
   
   def self.with_three_classifications
-        boats = ["Nacra 17", "Zodiac CZ7", "Sun Tracker Regency 254 XP3"].sort
+      #   boats = ["Nacra 17", "Zodiac CZ7", "Sun Tracker Regency 254 XP3"].sort
 
-      one = Classification.joins(:boats).where(boats: {name: "Nacra 17"})
-      two = Classification.joins(:boats).where(boats: {name: "Zodiac CZ7"})
-      three = Classification.joins(:boats).where(boats: {name: "Sun Tracker Regency 254 XP3"})
+      # one = Classification.joins(:boats).where(boats: {name: "Nacra 17"})
+      # two = Classification.joins(:boats).where(boats: {name: "Zodiac CZ7"})
+      # three = Classification.joins(:boats).where(boats: {name: "Sun Tracker Regency 254 XP3"})
 
-    self.joins(:classifications).where(classifications: {name: one.name})
+    self.joins(:classifications).where(boats: {name: ["Nacra 17", "Zodiac CZ7", "Sun Tracker Regency 254 XP3"]})
 
 
   end
