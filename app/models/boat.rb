afda9e76a6cb23839bd0f954c0ee74af0178fd16
Vishal Sharma <vsh1819@gmail.com>
2018-07-23 13:28:03 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -42,7 +42,7 @@ self.joins(:classifications).where(classifications: {name: "Sailboat"})
       two = Classification.joins(:boats).where(boats: {name: "Zodiac CZ7"})
       three = Classification.joins(:boats).where(boats: {name: "Sun Tracker Regency 254 XP3"})
 
-    self.joins(:classifications).where(classifications: {name: "Sailboat"})
+    self.joins(:classifications).where(classifications: {name: one.name})
 
 
   end
